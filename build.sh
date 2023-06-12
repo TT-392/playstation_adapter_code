65fdd66a5f9043edd0f416e2ce89783ef00572c7
TT-392 <git@tt-392.space>
2023-06-12 16:20:54 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PICO_SDK_PATH=../sdk/
+openocd_install_location=$HOME"/applications/openocd_rp2040/openocd"
+
+mkdir -p build && cd build
+cmake ..
+make -j$(cat /proc/cpuinfo | grep bogomips | wc -l)
+retval=$?
+cd ..
+
+
+if [ "$1" = "flash" ] && [ $retval -eq 0 ];
+then
+    #$openocd_install_location/src/openocd -s $openocd_install_location/tcl -f openocd/flash.cfg
+    picotool reboot -f -u
+    sleep 1
+    picotool load build/blink.uf2
+    picotool reboot
+fi
