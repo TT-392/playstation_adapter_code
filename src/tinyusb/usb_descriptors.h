62ac105ef2a8dd5d305e1e3faf7d41be9486c5c8
TT-392 <git@tt-392.space>
2023-08-25 21:55:20 +0200

    Keyboard HID recognized under windows

--- src/tinyusb/usb_descriptors.h
+++ src/tinyusb/usb_descriptors.h
@@ -25,24 +25,10 @@
 #ifndef USB_DESCRIPTORS_H_
 #define USB_DESCRIPTORS_H_
 
-extern uint8_t const desc_ms_os_20[];
-
 enum
 {
-  ITF_NUM_CDC = 0,
-  ITF_NUM_CDC_DATA, // just a placeholder to increment because cdc has 2 interface descriptors, I think?
-  ITF_NUM_KEYBOARD,
-  ITF_NUM_TOTAL
-};
-
-enum
-{
-  HID_INSTANCE_KEYBOARD = 0
-};
-
-enum
-{
-  CDC_INSTANCE_1 = 0,
+  REPORT_ID_KEYBOARD = 1,
+  REPORT_ID_COUNT
 };
 
 
